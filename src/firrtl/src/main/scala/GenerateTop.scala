7009a9246e432a3195f91ffc5165e3ebf215ecd3
Colin Schmidt <colins@eecs.berkeley.edu>
2016-10-28 16:58:10 -0700

    add clock list transform

--- src/firrtl/src/main/scala/GenerateTop.scala
+++ src/firrtl/src/main/scala/GenerateTop.scala
@@ -9,6 +9,7 @@ import firrtl.Annotations.AnnotationMap
 object FirrtlVerilogCompiler {
   val infer_read_write_id = TransID(-1)
   val repl_seq_mem_id     = TransID(-2)
+  val clock_list_id       = TransID(-3)
 }
 
 class EmitTopVerilog(topName: String) extends PLSIPassManager {
@@ -22,7 +23,8 @@ class EmitTopVerilog(topName: String) extends PLSIPassManager {
     )
 
   override def operateLow() = Seq(
-      new RemoveUnusedModules
+      new RemoveUnusedModules,
+      new passes.clocklist.ClockListTransform(FirrtlVerilogCompiler.clock_list_id)
     )
 }
 
@@ -32,6 +34,7 @@ object GenerateTop extends App {
   var synTop: Option[String] = None
   var harnessTop: Option[String] = None
   var seqMemFlags: Option[String] = Some("-o:unused.confg")
+  var listClocks: Option[String] = None
 
   var usedOptions = Set.empty[Integer]
   args.zipWithIndex.foreach{ case (arg, i) =>
@@ -56,6 +59,10 @@ object GenerateTop extends App {
         seqMemFlags = Some(args(i+1))
         usedOptions = usedOptions | Set(i+1)
       }
+      case "--list-clocks" => {
+        listClocks = Some(args(i+1))
+        usedOptions = usedOptions | Set(i+1)
+      }
       case _ => {
         if (! (usedOptions contains i)) {
           error("Unknown option " + arg)
@@ -73,6 +80,10 @@ object GenerateTop extends App {
       passes.InferReadWriteAnnotation(
         s"${synTop.get}",
         FirrtlVerilogCompiler.infer_read_write_id
+      ),
+      passes.clocklist.ClockListAnnotation(
+        s"${listClocks.get}",
+        FirrtlVerilogCompiler.clock_list_id
       ),
       passes.memlib.ReplSeqMemAnnotation(
         s"-c:${synTop.get}:${seqMemFlags.get}",
